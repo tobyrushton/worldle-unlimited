ca3956657240aea6dd794487ec2fc0ff6f58659f
Toby Rushton <tobyjrushton@icloud.com>
2022-05-09 16:22:30 +0100

    tests updated to include country area

--- src/__tests__/countries.test.ts
+++ src/__tests__/countries.test.ts
@@ -1,17 +1,17 @@
 import Country, { getCountry } from '../domain/countries'
 
 test('get country by name', ()=>{
-    expect(getCountry('United Arab Emirates')).toStrictEqual({country:"United Arab Emirates",alpha:"AE",latitude:24,longitude:54})
-    expect(getCountry('Christmas Island')).toStrictEqual({country:"Christmas Island",alpha:"CX",latitude:-10.5,longitude:105.6667})
-    expect(getCountry('Gabon')).toStrictEqual({country:"Gabon",alpha:"GA",latitude:-1,longitude:11.75})
-    expect(getCountry('Australia')).toStrictEqual({country:"Australia",alpha:"AU",latitude:-27,longitude:133})
+    expect(getCountry('United Arab Emirates')).toStrictEqual({country:"United Arab Emirates",alpha:"AE",latitude:24,longitude:54,area:83600})
+    expect(getCountry('Christmas Island')).toStrictEqual({country:"Christmas Island",alpha:"CX",latitude:-10.5,longitude:105.6667,area:135})
+    expect(getCountry('Gabon')).toStrictEqual({country:"Gabon",alpha:"GA",latitude:-1,longitude:11.75,area:267668})
+    expect(getCountry('Australia')).toStrictEqual({country:"Australia",alpha:"AU",latitude:-27,longitude:133,area:7741220})
 })
 
 test('get country by index',()=>{
-    expect(getCountry(134)).toStrictEqual({alpha: "MG",country: "Madagascar",latitude: -20,longitude: 47})
-    expect(getCountry(212)).toStrictEqual({alpha: "TO",country: "Tonga",latitude: -20,longitude: -175})
-    expect(getCountry(35)).toStrictEqual({alpha: "CC",country: "Cocos (Keeling) Islands",latitude: -12.5,longitude: 96.8333})
-    expect(getCountry(89)).toStrictEqual({alpha: "HK",country: "Hong Kong",latitude: 22.25,longitude: 114.1667})
+    expect(getCountry(134)).toStrictEqual({alpha: "MG",country: "Madagascar",latitude: -20,longitude: 47,area:587041})
+    expect(getCountry(212)).toStrictEqual({alpha: "TO",country: "Tonga",latitude: -20,longitude: -175,area:650})
+    expect(getCountry(35)).toStrictEqual({alpha: "CC",country: "Cocos (Keeling) Islands",latitude: -12.5,longitude: 96.8333,area:14})
+    expect(getCountry(89)).toStrictEqual({alpha: "HK",country: "Hong Kong",latitude: 22.25,longitude: 114.1667,area:1075})
 })
 
 test('get distance to country', ()=>{
